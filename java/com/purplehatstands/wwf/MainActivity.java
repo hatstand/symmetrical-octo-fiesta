43352f858ec4346105a49b993e7fa1b3dfe81123
John Maguire <john.maguire@gmail.com>
2016-11-17 16:32:23 +0000

    Render grid in Android view

--- java/com/purplehatstands/wwf/MainActivity.java
+++ java/com/purplehatstands/wwf/MainActivity.java
@@ -2,9 +2,15 @@ package com.purplehatstands.wwf;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.DataSetObserver;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
 import android.widget.TextView;
 import com.google.common.io.ByteStreams;
 import com.google.common.util.concurrent.FutureCallback;
@@ -22,12 +28,18 @@ import words.Service.Response;
 public class MainActivity extends Activity {
   private static final String TAG = "MainActivity";
 
+  private static final int GRID_SIZE = 15;
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
+    final LinearLayout layout = new LinearLayout(this);
+    layout.setOrientation(LinearLayout.VERTICAL);
+    setContentView(layout);
+
     final TextView view = new TextView(this);
-    setContentView(view);
+    layout.addView(view);
     view.setText("Thinking...");
 
     Intent intent = getIntent();
@@ -38,7 +50,7 @@ public class MainActivity extends Activity {
         InputStream stream = getContentResolver().openInputStream(imageUri);
         byte[] bytes = ByteStreams.toByteArray(stream);
         ManagedChannel channel =
-            ManagedChannelBuilder.forAddress("zaphod.purplehatstands.com", 32775)
+            ManagedChannelBuilder.forAddress("zaphod.purplehatstands.com", 32776)
                 .usePlaintext(true)
                 .build();
         CheaterGrpc.CheaterFutureStub stub = CheaterGrpc.newFutureStub(channel);
@@ -55,6 +67,8 @@ public class MainActivity extends Activity {
                       @Override
                       public void run() {
                         view.setText(response.toString());
+                        GridView grid = renderBoard(response.getBoard().getData());
+                        layout.addView(grid);
                       }
                     });
               }
@@ -68,5 +82,76 @@ public class MainActivity extends Activity {
         e.printStackTrace();
       }
     }
+  }
+
+  private GridView renderBoard(final ByteString data) {
+    GridView grid = new GridView(this);
+    grid.setNumColumns(GRID_SIZE);
+    grid.setAdapter(
+        new ListAdapter() {
+          @Override
+          public int getCount() {
+            return data.size();
+          }
+
+          @Override
+          public boolean areAllItemsEnabled() {
+            return true;
+          }
+
+          @Override
+          public boolean isEnabled(int position) {
+            return true;
+          }
+
+          @Override
+          public Object getItem(int position) {
+            return data.byteAt(position);
+          }
+
+          @Override
+          public long getItemId(int position) {
+            return position / GRID_SIZE;
+          }
+
+          @Override
+          public int getItemViewType(int position) {
+            return 0;
+          }
+
+          @Override
+          public View getView(int position, View convertView, ViewGroup parent) {
+            String text = Character.toString((char) data.byteAt(position));
+            if (convertView != null) {
+              ((TextView) convertView).setText(text);
+              return convertView;
+            }
+            TextView view = new TextView(MainActivity.this);
+            view.setText(text);
+            return view;
+          }
+
+          @Override
+          public int getViewTypeCount() {
+            return 1;
+          }
+
+          @Override
+          public boolean hasStableIds() {
+            return true;
+          }
+
+          @Override
+          public boolean isEmpty() {
+            return false;
+          }
+
+          @Override
+          public void registerDataSetObserver(DataSetObserver observer) {}
+
+          @Override
+          public void unregisterDataSetObserver(DataSetObserver observer) {}
+        });
+    return grid;
   }
 }
