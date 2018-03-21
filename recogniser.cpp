a1bfbf971eeb9a75f8796bf4dbe978ae3c901bfd
Clang Formatter <clang@clementine-player.org>
2018-03-21 21:48:59 +0000

    Automatically formatted

--- recogniser.cpp
+++ recogniser.cpp
@@ -1,38 +1,38 @@
 #include "recogniser.h"
 
-  using std::vector;
+using std::vector;
 
-  namespace {
-  static const int kGridSize = 15;
-  static const int kRackSize = 7;
-  }
+namespace {
+static const int kGridSize = 15;
+static const int kRackSize = 7;
+}  // namespace
 
-  Recogniser::Recogniser(const KNearest& nearest) : nearest_(nearest) {}
+Recogniser::Recogniser(const KNearest& nearest) : nearest_(nearest) {}
 
-  char Recogniser::Recognise(const cv::Mat& image) const {
-    return nearest_.Recognise(image);
-  }
+char Recogniser::Recognise(const cv::Mat& image) const {
+  return nearest_.Recognise(image);
+}
 
-  vector<char> Recogniser::RecogniseGrid(const cv::Mat& image) const {
-    const int grid_start = image.size().height / 4;
-    const int square_width = image.size().width / kGridSize;
+vector<char> Recogniser::RecogniseGrid(const cv::Mat& image) const {
+  const int grid_start = image.size().height / 4;
+  const int square_width = image.size().width / kGridSize;
 
-    vector<char> ret(kGridSize * kGridSize);
+  vector<char> ret(kGridSize * kGridSize);
 
-    for (int i = 0; i < kGridSize; ++i) {
-      for (int j = 0; j < kGridSize; ++j) {
-        int x = square_width * i;
-        int y = square_width * j + grid_start;
+  for (int i = 0; i < kGridSize; ++i) {
+    for (int j = 0; j < kGridSize; ++j) {
+      int x = square_width * i;
+      int y = square_width * j + grid_start;
 
-        cv::Point top_left(x, y);
-        cv::Point bottom_right(x + square_width, y + square_width);
+      cv::Point top_left(x, y);
+      cv::Point bottom_right(x + square_width, y + square_width);
 
-        cv::Mat square(image, cv::Rect(top_left, bottom_right));
-        ret[i + kGridSize * j] = Recognise(square);
-      }
+      cv::Mat square(image, cv::Rect(top_left, bottom_right));
+      ret[i + kGridSize * j] = Recognise(square);
     }
+  }
 
-    return ret;
+  return ret;
   }
 
   vector<char> Recogniser::RecogniseRack(const cv::Mat& image) const {
